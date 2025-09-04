076de365107cf5cfe4d07bbb42888899af28bdc9
minhthinh145 <minhthinh0384360149@gmail.com>
2025-09-04 22:31:14 +0700

    feat : add Interface RoomOrchestrator

---
+++ Backend/QLQuanKaraokeHKT/Core/Interfaces/Services/Room/IRoomPricingService.cs
@@ -0,0 +1,24 @@
+﻿using QLQuanKaraokeHKT.Core.Entities;
+
+namespace QLQuanKaraokeHKT.Core.Interfaces.Services.Room
+{
+    public interface IRoomPricingService
+    {
+        Task ApplyPricingConfigAsync(int maSanPham, IPricingConfig pricingConfig);
+        Task DisableCurrentPricesAsync(int maSanPham);
+        Task<List<GiaDichVu>> GetCurrentPricesAsync(int maSanPham);
+
+        IPricingConfig ExtractPricingConfig(List<GiaDichVu> prices);
+
+    }
+    public interface IPricingConfig
+    {
+        bool DongGiaAllCa { get; }
+        decimal? GiaThueChung { get; }
+        decimal? GiaThueCa1 { get; }
+        decimal? GiaThueCa2 { get; }
+        decimal? GiaThueCa3 { get; }
+        DateOnly NgayApDungGia { get; }
+        string TrangThaiGia { get; }
+    }
+}
